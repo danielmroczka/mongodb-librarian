d534f6568a45f68a9156dd8925936f7eb8d6d4fc
daniel <daniel.mroczka@gmail.com>
2013-11-30 11:01:13 +0100

    Exporter refactoring

--- src/main/java/com/labs/dm/mongodb/librarian/exporter/Exporter.java
+++ src/main/java/com/labs/dm/mongodb/librarian/exporter/Exporter.java
@@ -13,49 +13,65 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 /**
  * @author daniel
  */
 public class Exporter {
 
-    private final static String DIR = "D:\\dummy";
+    private static final Logger logger = Logger.getLogger("Exporter");
+    private final String dir;
+    private static final String mongoDB = "books";
+    private final DB db;
 
-    public static void main(String[] args) throws IOException {
-        MongoClientURI uri = new MongoClientURI("mongodb://wmb:wmb123@paulo.mongohq.com:10027/books");
-        MongoClient client = new MongoClient(uri);
-        DB db = client.getDB("books");
-        //DBCollection collection = db.getCollection("pdf3");
+    enum Mode {
 
-        Collection<File> pdfList = FileUtils.listFiles(new File(DIR), new String[]{"pdf"}, true);
-        Collection<File> mobiList = FileUtils.listFiles(new File(DIR), new String[]{"mobi"}, true);
-        Collection<File> epubList = FileUtils.listFiles(new File(DIR), new String[]{"epub"}, true);
-        System.out.println(pdfList.size());
-        System.out.println(epubList.size());
-        System.out.println(mobiList.size());
-        export(pdfList, "pdf", db.getCollection("pdf8"), 2);
-        //export(epubList, "epub", db.getCollection("epub"), 1);
-        //export(mobiList, "mobi", db.getCollection("mobi"), 1);
+        //Removes all documents and inserts 
+        DELETE_ADD,
+        //Adds only new documents
+        ADD,
+        //Adds new documents, remove unexistsing
+        ADD_REMOVE_ORPHANS
+    };
+
+    public Exporter(String mongoUri, String dir) throws UnknownHostException {
+        MongoClient client = new MongoClient(new MongoClientURI(mongoUri));
+        this.dir = dir;
+        db = client.getDB(mongoDB);
     }
 
-    private static void updateObject(DBObject item, File file, String ext) {
-        item.put("name", file.getName().substring(0, file.getName().indexOf("." + ext)));
-        item.put("ext", ext);
+    public static void main(String[] args) throws IOException {
+        Exporter exporter = new Exporter("mongodb://wmb:wmb123@paulo.mongohq.com:10027/books", "D:\\dummy");
+        exporter.execute();
+    }
+
+    private void execute() throws IOException {
+        Collection<File> ebookList = FileUtils.listFiles(new File(dir), new String[]{"pdf", "epub", "mobi"}, true);
+        export(ebookList, db.getCollection("ebook"), Mode.ADD_REMOVE_ORPHANS);
+    }
+
+    private void updateObject(DBObject item, File file) {
+        item.put("name", FilenameUtils.removeExtension(file.getName()));
+        item.put("ext", FilenameUtils.getExtension(file.getName()));
         item.put("path", file.getPath());
         item.put("size", file.length());
 
     }
 
-    private static void updateObject(DBObject item, PdfReader reader) {
+    private void updateObject(DBObject item, PdfReader reader) {
         item.put("pages", reader.getNumberOfPages());
         Map<String, String> map = new ConcurrentHashMap<>();
         map.putAll(reader.getInfo());
@@ -92,14 +108,14 @@ public class Exporter {
      * @param list
      * @param ext
      * @param collection
-     * @param mode 0-delete and insert, 1-update, 2-update and delete orphan
+     * @param mode
      */
-    public static void export(Collection<File> list, String ext, DBCollection collection, int mode) {
+    private void export(Collection<File> list, DBCollection collection, Mode mode) throws FileNotFoundException, IOException {
         int deleted = 0;
         int inserted = 0;
         int updated = 0;
         int counter = 0;
-        if (mode == 0) {
+        if (mode == Mode.DELETE_ADD) {
             collection.remove(new BasicDBObject());
         }
         Set<String> set = new HashSet<>();
@@ -107,45 +123,45 @@ public class Exporter {
             counter++;
             DBObject item = new BasicDBObject();
             PdfReader reader = null;
+            String md5 = DigestUtils.md5Hex(new FileInputStream(file));
+            item.put("_id", md5);
+            set.add(md5);
+            updateObject(item, file);
             try {
-                String md5 = DigestUtils.md5Hex(new FileInputStream(file));
-                item.put("_id", md5);
-                set.add(md5);
-                updateObject(item, file, ext);
                 reader = new PdfReader(file.getPath());
-                System.out.println(counter + "/" + list.size() + " " + reader.getInfo());
-                System.out.println(file.getPath());
                 updateObject(item, reader);
+                logger.info(counter + "/" + list.size() + " " + reader.getInfo());
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.severe(e.getMessage());
             } finally {
-                try {
-                    BasicDBObject query = new BasicDBObject("_id", item.get("_id"));
-                    if (mode == 0 || collection.find(query).size() == 0) {
-                        collection.insert(item);
-                        System.out.println("insert");
-                        inserted++;
-                    } else {
-                        updated++;
-                        System.out.println("update");
-                        collection.update(item, item);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
                 if (reader != null) {
                     reader.close();
                 }
+
+            }
+            logger.info(file.getPath());
+            BasicDBObject query = new BasicDBObject("_id", item.get("_id"));
+            if (mode == Mode.DELETE_ADD || collection.find(query).size() == 0) {
+                collection.insert(item);
+                inserted++;
+                logger.fine("insert");
+            } else {
+                collection.update(query, item);
+                updated++;
+                logger.fine("update");
             }
 
         }
-        if (mode == 2) {
+        if (mode == Mode.ADD_REMOVE_ORPHANS) {
             deleted = removeOrphans(set, collection);
         }
-        System.out.println("-------------------------------------------------");
-        System.out.println("Inserted items: " + inserted);
-        System.out.println("Updated items: " + updated);
-        System.out.println("Removed items: " + deleted);
+
+        StringBuilder report = new StringBuilder();
+        report.append("Summary report").append("\n");
+        report.append("Inserted items: ").append(inserted).append("\n");
+        report.append("Updated items: ").append(updated).append("\n");
+        report.append("Removed items: ").append(deleted).append("\n");
+        logger.info(report.toString());
     }
 
     private static int removeOrphans(Set<String> set, DBCollection collection) {
